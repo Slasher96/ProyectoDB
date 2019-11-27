36f7076eb54436511700b0ccce4277cbe5d4e18a
Antonio <acontrerasc96@hotmail.com>
2019-11-26 23:59:01 -0600

    cambios en vista y clase subsistemas model
    se agregan métodos para obtener ventanas y controles por id ventana y subsistema

--- MagicalLogger/Models/SubsistemaModel.cs
+++ MagicalLogger/Models/SubsistemaModel.cs
@@ -14,5 +14,49 @@ namespace MagicalLogger.Models
         public List<VentanaModel> ListaVentanas { get; set; }
 
         public bool EstaActivo { get; set; }
+
+
+        public List<VentanaModel> GetVentanasForIdsubsistema(int idSubsistema)
+        {
+            using (var context = new loggerBDEntities())
+            {
+                List<VentanaModel> ventanaslist = new List<VentanaModel>();
+                var listaventanasPorSubsistema = context.genSubsistemas.Include("genVentana").Where(a => a.estaActivo && a.idSubsistema == idSubsistema).ToList();
+                foreach (var item in listaventanasPorSubsistema)
+                {
+                    ventanaslist.Add(new VentanaModel
+                    {
+                        IdVentana = item.genVentana.idVentana,
+                        Descripcion = item.genVentana.descripcion,
+                        EsVentanaArranque = item.genVentana.esVentanaArranque
+                    });
+                }
+                return ventanaslist;
+            }
+        }
+
+
+        public List<Controles.ControlesModel> GetControlesForIdVentana(int idVentana)
+        {
+            using (var context = new loggerBDEntities())
+            {
+                var listacontroles = new List<Controles.ControlesModel>();
+                var listaControlesventana = context.genVentana.Include("regControles").Where(a => a.estaActivo && a.idVentana == idVentana).ToList();
+                foreach (var item in listaControlesventana)
+                {
+                    listacontroles.Add(new Controles.ControlesModel
+                    {
+                        IdControl = item.regcontroles.idControl,
+                        Descripcion = item.regcontroles.descripcion,
+                        TextoItemBoton = item.regcontroles.texto,
+                        TipoInteraccion = item.regcontroles.tipoInteraccion,
+                        IdAccion = item.regcontroles.idAccion
+                    });
+                }
+
+                return listacontroles;
+            }
+
+        }
     }
 }
